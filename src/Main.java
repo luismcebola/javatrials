f5639dae9eae8bc9b2f28f634d6fc7243e41bb08
Luis Cebola <luismcebola@gmail.com>
2022-11-10 14:44:26 +0000

    z included

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int x,y,z;
-        x = 20;
-        System.out.println("Hello world!");
-        for(int i=0;i<5;i++){
-            System.out.println("Hello world!");
+        int x[],i,j,key;
+        x={2,7,4,5,2,8};
+        for (i=0;i< x.length;i++){
+            key=x[i];
+            j=i-1;
+            while (j>=0 && x[j]>key){
+                x[j+1]=x[j];
+                j=j-1;
+            }
+            x[j+1]=key;
         }
+        System.out.println(x);
     }
 }
