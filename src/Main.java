ef1284fda4aa486a92e7766a20ef4f5bbb771b56
Luis Cebola <luismcebola@gmail.com>
2022-11-14 19:51:18 +0000

    exercises update

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        int x[],i,j,key;
-        x={2,7,4,5,2,8};
-        for (i=0;i< x.length;i++){
-            key=x[i];
-            j=i-1;
-            while (j>=0 && x[j]>key){
-                x[j+1]=x[j];
-                j=j-1;
-            }
-            x[j+1]=key;
-        }
-        System.out.println(x);
+    System.out.println("Hello");
     }
 }
+
