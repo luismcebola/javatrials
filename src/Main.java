c66ee353040c3bd3637d304723e49543d4378ddc
Luis Cebola <luismcebola@gmail.com>
2022-11-15 23:21:21 +0000

    print lists update

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-    System.out.println("Hello");
+        int i,j;
+        double key;
+        double[] X={0,1.4,10,4,7,6,1000, 9.3,6.4};
+        for (i=0;i< X.length;i++){
+            key=X[i];
+            j=i-1;
+            while (j>=0 && X[j]>key){
+                X[j+1]=X[j];
+                j=j-1;
+            }
+            X[j+1]=key;
+        }
+        System.out.println(Arrays.toString(X));
     }
 }
 
