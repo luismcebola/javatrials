965c7dc8d11d77b31d661a655441d22e5e870c7d
Luis Cebola <luismcebola@gmail.com>
2022-11-16 00:32:01 +0000

    print lists update

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
-import java.util.Arrays;
 public class Main {
-
     public static void main(String[] args) {
-        int i,j;
-        double key;
-        double[] X={0,1.4,10,4,7,6,1000, 9.3,6.4};
-        for (i=0;i< X.length;i++){
-            key=X[i];
-            j=i-1;
-            while (j>=0 && X[j]>key){
-                X[j+1]=X[j];
-                j=j-1;
-            }
-            X[j+1]=key;
+        double result = sq(3.4);
+        System.out.println(result);
+    }
+    public static double sq(double k) {
+        if (k > 1) {
+            return sq(k-1) + k + k - 1;
+        } else {
+            return k*k;
         }
-        System.out.println(Arrays.toString(X));
     }
 }
-
